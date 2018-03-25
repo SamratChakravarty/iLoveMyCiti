1f3ddf59f4e863590ddec56c0180513ee650d0b9
SamratChakravarty <samrat6feb@gmail.com>
2018-03-26 01:19:49 +0800

    first bootstrap project

---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require('express');
+
+var app = express();
+
+app.set('view engine','ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/:city',function(req,res){
+var imageCount=4;
+var cityLabel="";
+var city = req.params.city;
+
+if(city=='bangaluru'){
+	cityLabel="Bangaluru";
+}
+else if (city=='singapore'){
+	cityLabel = "Singapore";
+}
+else if (city == 'newyork'){
+	cityLabel = "New York";
+}
+
+	res.render('city',
+			  {city: req.params.city,
+			   title:"EJS Demo",
+			   cityLabel:cityLabel,
+			   imageCount:imageCount
+			  });
+});
+
+app.get('/',function(req,res){
+
+	res.render('home',
+			    { headline: "EJS Demo",
+			      title: "EJS Demo "
+			    });
+
+});
+
+app.listen(3000, function(){
+	console.log('Server is up and running on port 3000');
+
+});
